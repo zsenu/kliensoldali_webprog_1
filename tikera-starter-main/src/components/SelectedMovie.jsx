31fd4a4e62a12ad18e0ef185344007810acdabf0
zsenu <gstqli@inf.elte.hu>
2025-05-06 01:17:33 +0200

    moved images to public folder for vercel deployment

--- tikera-starter-main/src/components/SelectedMovie.jsx
+++ tikera-starter-main/src/components/SelectedMovie.jsx
@@ -13,7 +13,7 @@ const SelectedMovie = ({ movie, selectedDay }) => {
             borderRadius: "10px",
         }}>
             <img
-                src = {`./src/assets/images/${movie.image}`}
+                src = {`public/images/${movie.image}`}
                 alt = {`image for ${movie.title}: ${movie.image}`}
                 style =
                 {{
